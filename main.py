7f1a53ffdc3611a49f6dbc73c61fda63dae80fd2
mauricioBucerius <52835194+mauricioBucerius@users.noreply.github.com>
2023-03-22 15:45:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
 from rename_files import rename_files
 
-# ADAR4002_g1_files_index_mode0_initial
-PATH_REMOVE = 'INSERT PATH'
-DATATYPE = 'INSERT DATATYPE'
+PATH_REMOVE = 'Insert Path'
+FOLDER = 'Insert Folder'
 
-FOLDER = 'INSERT FOLDER'
+DELIMITER = 'Insert Delimiter'
+DATATYPE = 'Insert Datatype'
+
+KEY_NAME_1 = 'Insert Key Name 1'
+KEY_NAME_2 = 'Insert Key Name 2'
+START_KEY_1 = int(FOLDER.split(DELIMITER)[-1])
+START_KEY_2 = 0
+
+HEADER = 'BEGIN DSCRDATA\n% INDEX file\n' # for ADS
+
 FILENAME = f"INSERT BASEFILENAME {os.path.basename(FOLDER).lower().replace(' ', '_')}.txt"
 
-KEY1 = 'KEY NAME 1'
-KEY2 = 'KEY NAME 2'
-START_KEY_1 = int(FOLDER.split('_')[-1])
-START_KEY_2 = 0
 
 def get_int_key(string, key):
     for part in string.split('_'):
@@ -29,8 +33,8 @@ def get_file(file, idx, key):
     return '_'.join(new_file_list)
 
 def get_key(string):
-    key_1 = get_int_key(string, KEY1)
-    key_2 = get_int_key(string, KEY2)
+    key_1 = get_int_key(string, KEY_NAME_1)
+    key_2 = get_int_key(string, KEY_NAME_2)
     # print(string, key_1, key_2, key_1 == 0, key_2 != 0)
     if key_1 == START_KEY_1 and key_2 == START_KEY_2:
         return START_KEY_1
@@ -62,14 +66,11 @@ def main():
     
     for entry in os.listdir():
         if entry.split('.')[-1].lower() == DATATYPE:
-            # print(f'key 1: {get_int_key(entry, KEY1)} - key 2: {get_int_key(entry, KEY2)}')
             
-            if get_int_key(entry, KEY1) > START_KEY_1:
-                # print(f' {entry} - into key 1:  {get_key(entry)}')
+            if get_int_key(entry, KEY_NAME_1) > START_KEY_1:
                 files_key_1.append(entry)
                                     
-            elif get_int_key(entry, KEY2) > START_KEY_2:
-                # print(f' {entry} - into key 2:  {get_key(entry)}')
+            elif get_int_key(entry, KEY_NAME_2) > START_KEY_2:
                 files_key_2.append(entry)
                                     
             elif len(files_key_1) == 0 and len(files_key_2) == 0:
@@ -82,12 +83,12 @@ def main():
     print(f'Key 1 list entries: {len(files_key_1)} \t Key 2 list entries: {len(files_key_2)}')
     with open(FILENAME, 'w') as file:
         print(f'Opens File: {FILENAME}')
-        file.write('BEGIN DSCRDATA\n% INDEX file\n')
+        file.write(HEADER)
         
         idx = 0
         if len(files_key_1) > 1:
             for entry in files_key_1:
-                cur_idx_key_1 = get_int_key(entry, KEY1)
+                cur_idx_key_1 = get_int_key(entry, KEY_NAME_1)
                 
                 path = get_path(entry)
                 print(f'Writes Line: {idx: <3}{path}')
@@ -99,7 +100,7 @@ def main():
                 else:
                     file.write(f'{idx: <4}{path}\n')
                     idx += 1
-                    files_miss.append(get_file(entry, idx_key_1, KEY1))
+                    files_miss.append(get_file(entry, idx_key_1, KEY_NAME_1))
                     idx_key_1 = cur_idx_key_1 + 1
                     cnt_miss += 1
                 idx += 1
@@ -108,20 +109,19 @@ def main():
             
         if len(files_key_2) > 1:
             for entry in files_key_2:
-                cur_idx_key_2 = get_int_key(entry, KEY2)
+                cur_idx_key_2 = get_int_key(entry, KEY_NAME_2)
                 
                 path = get_path(entry)
                 print(f'Writes Line: {idx: <3}{path}')
                 file.write(f'{idx: <4}{path}\n')
                 idx += 1
                 
-                # print(cur_idx_key_2, idx_key_2, cur_idx_key_2 == idx_key_2)
                 if cur_idx_key_2 == idx_key_2:
                     idx_key_2 += 1
                 else:
                     file.write(f'{idx: <4}{path}\n')
                     idx += 1
-                    files_miss.append(get_file(entry, idx_key_2, KEY2))
+                    files_miss.append(get_file(entry, idx_key_2, KEY_NAME_2))
                     idx_key_2 = cur_idx_key_2 + 1
                     cnt_miss += 1
         else:
@@ -142,8 +142,7 @@ if __name__ == '__main__':
     if not os.path.basename(os.getcwd()) == os.path.basename(FOLDER):
         path = '\\'.join([os.getcwd(), FOLDER])
         os.chdir(path)
-        
-    # only have to rename, when the naming convention in the file fails somehow
-    # rename_files(KEY1, START_KEY_1, DATATYPE)
+    # only have to rename, when the naming convention in the measureming fails
+    # rename_files(KEY_NAME_1, START_KEY_1, DATATYPE)
     
     main()
